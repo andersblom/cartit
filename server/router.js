8a0648f2256bef1acde1c1d729b5f0fc270615c4
Anders Blom <anders@andersblom.dk>
2017-07-28 18:13:57 +0200

    some list-logic in router

--- server/router.js
+++ server/router.js
@@ -2,6 +2,8 @@
 var express = require("express");
 var router = express.Router();
 const User = require("./model/user");
+const List = require("./model/list");
+const ListEntry = require("./model/listentry");
 
 router.route('/user/:username')
     .get((req, res) => {
@@ -52,9 +54,30 @@ router.route('/user/:username')
 
 router.route('/user/:username/list/:id')
     .get((req, res) => {
-        res.json({
-            message: "got GET request"
-        })
+        User.findOne({ 'username': req.params.username }, (err, user) => {
+            if (err) {
+                res.send(err);
+            }
+            if (user) {
+                List.findOne({ 'listId': req.params.id }, (err, list) => {
+                    if (list) {
+                        res.json(list);
+                    } else {
+                        res.status(400).json({
+                            error: {
+                                message: "List was not found"
+                            } 
+                        });
+                    }
+                });
+            } else {
+                res.status(400).json({
+                   error: {
+                       message: "User was not found"
+                   } 
+                });
+            }
+        });
     })
     .put((req, res) => {
         res.json({
