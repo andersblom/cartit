3146f01d6c644b72b4fc1d3bd0d3884c062e5243
Anders Blom <anders@andersblom.dk>
2017-07-28 15:22:14 +0200

    environment stuff

--- server/server.js
+++ server/server.js
@@ -1,3 +1,22 @@
 'use strict';
 
-const dbConfig = require('./dbConfig');
+// Modules
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+// const TodoUser = require("./model/todouser");
+
+// Configs
+const dbConfig = require("./dbConfig");
+
+const app = express();
+const router = express.Router();
+
+const port = process.env.API_PORT || 3001;
+
+mongoose.connect(`mongodb://${dbConfig.user}:${dbConfig.password}@${dbConfig.url}:${dbConfig.port}/${dbConfig.name}`);
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
