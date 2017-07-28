d9c58d35e3cdc0121b2f5cbb1d441ddc2a99300f
Anders Blom <anders@andersblom.dk>
2017-07-28 14:59:46 +0200

    project structure stuff

---
+++ server/config-example.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const config = {
+    dbUrl: '<DB URL>', // mongodb:// is added in server.js
+    dbPort: 00000,
+    dbUser: '<DB USER>',
+    dbPassword: '<DB PASSWORD>',
+    dbName: '<DB NAME>'
+}
