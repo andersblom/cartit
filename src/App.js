cbc7bb29c5037c3d09b84538ba84ba7361a7aa3e
Anders Blom <anders@andersblom.dk>
2017-08-01 19:18:36 +0200

    React frontend setup + react router install

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -22,6 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        
       </div>
     );
   }
