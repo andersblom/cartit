75da065c91ba3da4b529b9c29881e784424d9e4b
Anders Blom <anders@andersblom.dk>
2017-08-22 23:00:07 +0200

    cleaning up

--- src/components/ShowAllLists/ShowAllLists.js
+++ src/components/ShowAllLists/ShowAllLists.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import _map from 'lodash.map';
+import _ from 'lodash';
 
 import SingleListItem from '../SingleListItem/SingleListItem';
 
 export default class ShowAllLists extends Component {
     render() {
-        const listsToBeRendered = _map(this.props.user.lists, list => {
+        const listsToBeRendered = _.map(this.props.user.lists, list => {
             return (<Link to={`${this.props.match.url}/list/${list._id}`} key={list._id}><SingleListItem listProps={list} /></Link>);
         });
         return(
