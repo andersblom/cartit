0f8a9d0eef45e730d1d41e5c564d8268837aa7c4
Anders Blom <anders@andersblom.dk>
2017-09-02 13:17:40 +0200

    Single shopping item css

--- src/components/SingleListView/_Item/_Item.js
+++ src/components/SingleListView/_Item/_Item.js
@@ -4,8 +4,10 @@ import _ from 'lodash';
 export default class _Item extends Component {
     render() {
         return(
-            <div>
-                [ ] {this.props.itemName}
+            <div className="singleShoppingListItem">
+                <div className="singleShoppingListItem-icon">(m)</div>
+                <div className="singleShoppingListItem-itemName">{this.props.itemName}</div>
+                <div className="singleShoppingListItem-status">{this.props.statusIsDone? ":)":":("}</div>
             </div>
         )
     }
